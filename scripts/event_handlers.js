def56254fa6cec2dcf99b3e6606c501949bdcdbc
OVECJOE <ovecjoe123@gmail.com>
2021-11-22 14:12:34 +0100

    Adjusted something I don't even remember, but I guess it has something to do with colors

--- scripts/event_handlers.js
+++ scripts/event_handlers.js
@@ -1,7 +1,7 @@
 function getSearchQuery() {
 	var query = document.getElementById("input").value;
 
-	let idx = query.search(/^([A-Z]*\/\s)?\{[a-zA-Z]+ '([a-zA-Z0-9]*.){1,}'\}/g);
+	let idx = query.search(/^([A-Z]*\/\s)?\{[a-zA-Z]+ ["']([a-zA-Z0-9]*.){1,}["']\}/g);
 	if (idx)
 	{
 		alert("SyntaxError: Search Query (" + query + ") is Invalid!");
