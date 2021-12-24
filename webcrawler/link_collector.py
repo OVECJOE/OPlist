f000b5273e67c6de8a9d45460b1fd3d37e3c6a02
OVECJOE <ovecjoe123@gmail.com>
2021-12-24 08:55:03 +0100

    Made changes to link_collector.py... :hammer:

--- webcrawler/link_collector.py
+++ webcrawler/link_collector.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-from urllib.error import URLError
 from urllib.parse import urlparse
 import requests
 from requests.exceptions import ConnectionError, InvalidURL
@@ -13,60 +12,106 @@ class LinkCollector:
     content-related links
     """
 
-    def __init__(self, title=""):
+    def __init__(self):
         """Initializes the link collector object/instance"""
-        self.title = title
         self.__seedURLs = [
             'https://www.codementor.io/projects',
-            'https://www.geeksforgeeks.org/',
-            'https://www.freecodecamp.org/learn/',
-            'https://www.w3schools.com/',
-            'https://developer.mozilla.org/en-US/'
+            'https://www.geeksforgeeks.org',
+            'https://www.freecodecamp.org/learn',
+            'https://www.w3schools.com',
+            'https://developer.mozilla.org/en-US',
+            'https://codeforgeek.com',
+            'https://www.fullstackpython.com',
+            'https://github.com',
+            'https://dev.to',
+            'https://blog.feedspot.com/programming_blogs'
         ]
-        self.gatheredLinks = []
+        self.__collectedLinks = set()
+        self.__visitedLinks = set()
         self.__urlPattern = re.compile(r'^((http(s)?)://)(www.)?\S+')
         self.__count = 0
+
+    @property
+    def collectedLinks(self):
+        return self.__collectedLinks
+    
+    @property
+    def visitedLinks(self):
+        return self.__visitedLinks
 
     def getPage(self, url):
         """Gets the web page of a url containing the code."""
         try:
-            newUrl = "{}://{}".format(urlparse(url).scheme,
-                                      urlparse(url).netloc)
-            response = requests.get(newUrl, allow_redirects=True)
+            response = requests.get(url, allow_redirects=True)
             if response.status_code == 200:
+                self.__visitedLinks.add(response.url)
                 return response.text
-        except (ConnectionError, InvalidURL) as e:
-            print("Error:", e)
+        except (ConnectionError, InvalidURL):
+            print("Couldn't collect links from {}. I would try another URL..."
+                  .format(url))
         except KeyboardInterrupt:
-            print('\n\n', self.gatheredLinks)
+            print("============RESULT=============\n")
+            for result in self.__collectedLinks:
+                print("\t{}".format(result[0]))
+                print("\t\t{}".format(result[1]))
+            print("\n\nI am smart. I collected {} links and visited {} URLs!"
+                  .format(len(self.__collectedLinks), len(self.__visitedLinks)))
+
+    def __extractPageTitle(self, link):
+        """Extracts the page's title for indexing purpose, maybe."""
+        html = self.getPage(link)
+        if html is not None:
+            print("Extracting title from webpage (location: {}, time: {})"
+                  .format(link, datetime.now().strftime("%H:%M:%S")))
+            bs = BeautifulSoup(html, 'html.parser')
+            try:
+                title = bs.head.title.text
+                return title
+            except AttributeError:
+                title = bs.title.text
+                return title if title else ""
+
+    def __checkLink(self, link):
+        """Checks that certain requirements are met for a link"""
+        if link in self.__visitedLinks and \
+                link in self.__collectedLinks:
+            print("Removing link ({})...".format(link))
+            self.__collectedLinks.discard(link)
+
+        nonDuplicates = [tup[1] for tup in self.__collectedLinks]
+        if self.getPage(link) is not None and link not in nonDuplicates:
+            title = self.__extractPageTitle(link)
+            print("Adding ({}, {}) to the list of collected links..."
+                  .format(title, link))
+            self.__collectedLinks.add((title, link, datetime.now()))
 
     def parsePage(self, url):
-        """Uses BeautifulSoup to parse the html page loaded"""
+        """Parses the html page returned by getPage and stores it as a dict"""
         self.__count += 1
-        print("Targeting url ({}) at {} --- [{:d}]".
-              format(url, datetime.now(), self.__count))
+        print("Targeting url ({}) at {} --- [{:d}]".format(
+            url,
+            datetime.now().
+            strftime('Date: %d %b, %Y; Time: %H:%M:%S'),
+            self.__count))
         html = self.getPage(url)
-        if html is None:
-            return
-        links = []
-        bs = BeautifulSoup(html, 'html.parser')
-        for link in bs.findAll('a'):
-            if 'href' in link.attrs and link not in links:
-                links.append(link.attrs['href'])
-        for link in links:
-            if self.__urlPattern.match(link):
-                link = "{}://{}{}".format(
-                    urlparse(link).scheme,
-                    urlparse(link).netloc,
-                    urlparse(link).path
-                )
-                if link not in self.gatheredLinks:
-                    self.gatheredLinks.append(link)
-            else:
-                newUrl = "{}://{}".format(urlparse(url).scheme,
-                                          urlparse(url).netloc) + link
-                if newUrl not in self.gatheredLinks:
-                    self.gatheredLinks.append(newUrl)
+        if html is not None:
+            print("Parsing Page...")
+            bs = BeautifulSoup(html, 'html.parser')
+            for a in bs.findAll('a'):
+                if all(['href' in a.attrs,
+                       a.attrs.get('href', '')
+                        not in [tup[1] for tup in self.__collectedLinks]]):
+                    link = a.attrs['href']
+                    if self.__urlPattern.match(link):
+                        link = "{}://{}{}".format(
+                            urlparse(link).scheme,
+                            urlparse(link).netloc,
+                            urlparse(link).path
+                        )
+                    else:
+                        link = "{}://{}".format(urlparse(url).scheme,
+                                                urlparse(url).netloc) + link
+                    self.__checkLink(link)
 
     def loopThruSeedUrls(self, seedUrls=[]):
         """Goes through all the seed URLs and extract links"""
@@ -74,12 +119,3 @@ class LinkCollector:
             self.__seedURLs = seedUrls
         for url in self.__seedURLs:
             self.parsePage(url)
-
-
-# if __name__ == '__main__':
-#     link_collector = LinkCollector()
-#     link_collector.loopThruSeedUrls()
-#     links = link_collector.gatheredLinks
-#     print(len(links))
-#     link_collector.loopThruSeedUrls(links)
-#     print(len(link_collector.gatheredLinks))
